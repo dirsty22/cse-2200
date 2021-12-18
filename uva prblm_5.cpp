97be96f13a10046d5806734d24ceaf9e653cbfc5
dirsty22 <71918113+dirsty22@users.noreply.github.com>
2021-12-18 12:35:56 +0600

    Add files via upload

---
+++ uva prblm_5.cpp
@@ -0,0 +1,18 @@
+//uva problem no 10055
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int a,b;
+    while(cin>>a >>b){
+        if(a>b)
+        {
+          cout<<a-b<<endl;
+        }
+        else
+        {
+            cout<<b-a<<endl;
+        }
+    }
+    return 0;
+}
